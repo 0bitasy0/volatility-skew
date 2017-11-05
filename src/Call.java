ba3d770ff9463a169b6cce0cd6ff4c9bd33cd988
Brian Nieves <0bitasy0@gmail.com>
2017-11-05 01:50:47 -0500

    Optimized code written, but not debugged. Still incomplete.

--- src/Call.java
+++ src/Call.java
@@ -2,6 +2,38 @@
 import com.opencsv.bean.CsvBindByName;
 
 public class Call {
+    public String getSecid() {
+        return secid;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDays() {
+        return days;
+    }
+
+    public String getImpl_volatility() {
+        return impl_volatility;
+    }
+
+    public String getImpl_strike() {
+        return impl_strike;
+    }
+
+    public String getCp_flag() {
+        return cp_flag;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public String getIndex_flag() {
+        return index_flag;
+    }
+
     @CsvBindByName
     private String secid;
 
--- src/Driver.java
+++ src/Driver.java
@@ -1,38 +1,98 @@
-import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Driver {
-    private static final String CALL_SAMPLE = "data/CallSample2.csv";
-    private static final String PRICE_SAMPLE = "data/PriceSampleData.csv";
+    private static final String CALL_FILE = "data/CallSample.csv";
+    private static final String PRICE_FILE = "data/PriceSampleData.csv";
+    //private static final String PRICE_FILE = "data/PRICEDATASP500.csv";
 
     public static void main(String[] args){
-        List<Call> result = null;
+        Scanner calls;
+        List<String[]> prices;
         try {
-            result = new CsvToBeanBuilder(new FileReader(CALL_SAMPLE)).withType(Call.class).build().parse();
-        } catch (FileNotFoundException e) {
-            System.out.println("Couldn't find file");
-            e.printStackTrace();
+            calls = new Scanner(new File(CALL_FILE)).useDelimiter("[\\n,]");
+            CSVReader reader = new CSVReaderBuilder(new FileReader(PRICE_FILE)).withCSVParser(new RFC4180Parser()).build();
+            prices = reader.readAll();
+        } catch (IOException e) {
+            System.out.println("Couldn't find files");
+            throw new RuntimeException(e.getMessage());
+        }
+
+        calls.nextLine(); //Skip headers
+        String[] companies = prices.get(0);
+        String[][] callVolatilities = new String[prices.size()][companies.length];
+
+        System.arraycopy(companies, 0, callVolatilities[0], 0, companies.length);
+        for(int i = 0; i < prices.size(); i++){
+            callVolatilities[i][0] = prices.get(i)[0];
+        }
+
+        int numCompanies = companies.length;
+        calls.next(); //Skip secid
+        int currentDate = Integer.parseInt(calls.next());
+        int currentYear = currentDate/10000;
+        int date = currentDate;
+        int rowBeforeYear = 1;
+        int currentRow;
+        year:
+        while(calls.hasNext()) {
+            company:
+            for(int j = 1; j < numCompanies; j++){
+                String currentCompany = prices.get(0)[j];//TODO: Add checker to make sure next company has any entries in a given year (otherwise skip), make sure no data is entered for callVolatilities if min is -1 at the end (currently puts 0 inside).
+                String company = currentCompany;
+                currentRow = rowBeforeYear;
+                    while(company.equals(currentCompany)) {
+                    double min = -1;
+                    double minVol = 0;
+                    while (currentDate == date) {
+                        calls.next(); //Skip days
+                        double volatility = calls.nextDouble();
+                        double strike = calls.nextDouble();
+                        double diff = Math.abs(Double.parseDouble(prices.get(currentRow)[j]) - strike);
+                        if(min == -1 && diff < min){
+                            min = diff;
+                            minVol = volatility;
+                        }
+                        calls.next(); //Skip flag
+                        company = calls.next();
+                        calls.next(); //Skip index
+                        calls.next(); //Skip secid
+                        date = calls.nextInt();
+                    }
+                    callVolatilities[currentRow][j] = minVol+"";
+                    if (date / 10000 != currentYear) {
+                        rowBeforeYear = currentRow;
+                        currentYear = date / 10000;
+                        while(Integer.parseInt(prices.get(currentRow)[0]) != date) currentRow++;
+                        continue year;
+                    } else if (!company.equals(currentCompany)) {
+                        continue company;
+                    }
+                    while(Integer.parseInt(prices.get(currentRow)[0]) != date) currentRow++;
+
+                }
+            }
         }
 
 
-        loopingStrategy();
     }
 
     private static void loopingStrategy() {
         try {
-            Scanner callSampleScanner = new Scanner(new File(CALL_SAMPLE));
+            Scanner callSampleScanner = new Scanner(new File(CALL_FILE));
             callSampleScanner.useDelimiter(",");
-            Scanner priceSampleScanner = new Scanner(new File(PRICE_SAMPLE));
+            Scanner priceSampleScanner = new Scanner(new File(PRICE_FILE));
             priceSampleScanner.useDelimiter(",");
 
-            int rows = getNumRows(new Scanner(new File(CALL_SAMPLE)));
+            int rows = getNumRows(new File(CALL_FILE));
             String[][] callSample = loadData(callSampleScanner, rows);
-            rows = getNumRows(new Scanner(new File(PRICE_SAMPLE)));
+            rows = getNumRows(new File(PRICE_FILE));
             String[][] priceSampleData = loadData(priceSampleScanner, rows);
 
             double[][] answers = new double[priceSampleData.length][priceSampleData[0].length];
@@ -61,12 +121,20 @@ public class Driver {
         }
     }
 
-    private static int getNumRows(Scanner scanner) {
+    private static int getNumRows(File file) {
         int count = 0;
+        Scanner scanner;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Couldn't find file");
+            throw new RuntimeException(e.getMessage());
+        }
         while(scanner.hasNextLine()){
             count++;
             scanner.nextLine();
         }
+        scanner.close();
         return count;
     }
 
