27ae932ae0ff6986f42e17e55f200f13da15352a
Brian Nieves <0bitasy0@gmail.com>
2017-11-11 22:57:44 -0500

    Added commentation

--- src/Driver.java
+++ src/Driver.java
@@ -13,10 +13,15 @@ public class Driver {
     public static final String DATA_PUT_OUTPUT_CSV = "data/PutOutput.csv";
     private static final String CALL_FILE = "data/putsdata.csv";
     private static final String PRICE_FILE = "data/pricedata.csv";
+    public static final double CALL_PUT_MULTIPLIER = 0.95;
 
     public static void main(String[] args){
         Scanner calls;
         List<String[]> prices;
+
+        /*
+        Setup the scanner for calls/puts and create the table of prices.
+         */
         try {
             calls = new Scanner(new File(CALL_FILE)).useDelimiter("[\\n,]");
             CSVReader reader = new CSVReaderBuilder(new FileReader(PRICE_FILE)).withCSVParser(new RFC4180Parser()).build();
@@ -26,10 +31,13 @@ public class Driver {
             throw new RuntimeException(e.getMessage());
         }
 
-        calls.nextLine(); //Skip headers
-        String[] companies = prices.get(0);
-        String[][] callVolatilities = new String[prices.size()][companies.length];
+        calls.nextLine(); //Skip call/put headers
+        String[] companies = prices.get(0); //Get the top row of the prices file, which contains the companies
+        String[][] callVolatilities = new String[prices.size()][companies.length]; //Create the array which will be written to the output file.
 
+        /*
+        Add the first row or the prices table (the companies) to the output array
+         */
         System.arraycopy(companies, 0, callVolatilities[0], 0, companies.length);
         for(int i = 0; i < prices.size(); i++){
             callVolatilities[i][0] = prices.get(i)[0];
@@ -38,20 +46,26 @@ public class Driver {
         Call current = null;
         int currentRow = 0;
 
+        /*
+        Get to the first existing call/put entry.
+         */
         while(current == null){
             current = nextCall(calls);
         }
 
         loop:
-        for(int i = 1; i < companies.length; i++){
-            currentRow = getCurrentRow(1, current, prices);
-            String currentCompany = current.company;
-            if(!prices.get(0)[i].equals(currentCompany)) continue;
-            while(current.company.equals(currentCompany)){
+        for(int i = 1; i < companies.length; i++){ //For every company...
+            currentRow = getCurrentRow(1, current, prices); //Find out which row of the output we're on
+            String currentCompany = current.company; //Get the company of the current entry
+            if(!prices.get(0)[i].equals(currentCompany)) continue; //If the company we're on doesn't match the entry's, skip to the next company. This happens when an entire company has no valid entries.
+            while(current.company.equals(currentCompany)){ //While the next entry is still one from the same company we're on...
                 int currentDate = current.date;
                 double min = -1;
                 double minVol = 0;
                 String priceString;
+                /*
+                Search for a valid entry that has a matching valid price associated with it.
+                 */
                 if((priceString = prices.get(currentRow)[i]).equals("")){
                     do {
                         current = nextCall(calls);
@@ -59,8 +73,11 @@ public class Driver {
                     currentRow = getCurrentRow(currentRow, current, prices);
                     continue;
                 }
-                double price = Double.parseDouble(priceString)*0.95;
-                while (current.date == currentDate) {
+                double price = Double.parseDouble(priceString)* CALL_PUT_MULTIPLIER;
+                /*
+                Look through every entry with the same date and keep track of the minimum strike entry and its associated volatility.
+                 */
+                while (current.date == currentDate) { //Potential bug: If one company's last entry is on the same date as the next company's first entry, this loop will treat those separate sections as one.
                     double diff = Math.abs(price - current.strike);
                     if(min == -1 || diff < min){
                         min = diff;
@@ -71,13 +88,16 @@ public class Driver {
                         current = nextCall(calls);
                     } while(current == null);
                 }
-                callVolatilities[currentRow][i] = min == -1 ? "" : minVol+"";
-                currentRow = getCurrentRow(currentRow, current, prices);
-                if(currentRow == -1)
+                callVolatilities[currentRow][i] = min == -1 ? "" : minVol+""; //We've reached the end of the section, so record the volatility in the output array.
+                currentRow = getCurrentRow(currentRow, current, prices); //Get ready to look at the next entry.
+                if(currentRow == -1) //This occurs when we hit a new company. The loop breaks out of the day-by-day cycle to the next company.
                     break;
             }
         }
 
+        /*
+        Write the output array to a file.
+         */
         try {
             CSVWriter writer = new CSVWriter(new FileWriter(DATA_PUT_OUTPUT_CSV));
             for(String[] row : callVolatilities)
